9e76f984396636975186abba7b8648e6fa06ae29
Conior <970239641@qq.com>
2019-04-16 23:43:28 +0800

    Struts2

---
+++ Struts2-2/src/com/wsj/struts2/helloworld/Product.java
@@ -0,0 +1,44 @@
+package com.wsj.struts2.helloworld;
+
+public class Product {
+	private Integer productId;
+	private String productName;
+	private String productDesc;
+	private Double productPrice;
+	
+	public Integer getProductId() {
+		return productId;
+	}
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getProductDesc() {
+		return productDesc;
+	}
+	public void setProductDesc(String productDesc) {
+		this.productDesc = productDesc;
+	}
+	public Double getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(Double productPrice) {
+		this.productPrice = productPrice;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [productId=" + productId + ", productName=" + productName + ", productDesc=" + productDesc
+				+ ", productPrice=" + productPrice + "]";
+	}
+	
+	public String save() {
+		System.out.println("save" + this);
+		return "detais";
+	}
+}
